3d83e023aa577a745652b0727b350ad3f8e73ba4
daniel <46127670+danieli1818@users.noreply.github.com>
2025-08-04 11:05:15 +0300

    Update paths to use the example dataset as default

--- bacondistance/utils/paths.py
+++ bacondistance/utils/paths.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 PROJECT_ROOT_DIR_PATH = Path(__file__).parent.parent.parent
 
-DEFAULT_DATASET_PATH = PROJECT_ROOT_DIR_PATH / "datasources" / "dataset.json"
+DEFAULT_DATASET_PATH = PROJECT_ROOT_DIR_PATH / "examples" / "dataset_example.json"
 
 FRONTEND_DIR_PATH = PROJECT_ROOT_DIR_PATH / "frontend"
