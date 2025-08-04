a1fa64cb42918c97e82854de7d39143eeb84a050
daniel <46127670+danieli1818@users.noreply.github.com>
2025-08-04 10:38:09 +0300

    Add paths consts

---
+++ bacondistance/utils/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT_DIR_PATH = Path(__file__).parent.parent.parent
+
+DEFAULT_DATASET_PATH = PROJECT_ROOT_DIR_PATH / "datasources" / "dataset.json"
+
+FRONTEND_DIR_PATH = PROJECT_ROOT_DIR_PATH / "frontend"
