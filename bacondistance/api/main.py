5079e654a3a454f4b4f203f1b7aa7cd01cc88b21
daniel <46127670+danieli1818@users.noreply.github.com>
2025-08-13 22:43:13 +0300

    Fix get function bug

--- bacondistance/api/main.py
+++ bacondistance/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -6,7 +6,7 @@ from ..scripts.bacon_distance import calc_bacon_distance
 from ..utils.exceptions import ActorNotFoundError
 from .lifespan import lifespan
 from .paths import FRONTEND_PATH
-from .schemes import BaconDistanceRequest, BaconDistanceResponse, ErrorResponse
+from .schemes import BaconDistanceResponse, ErrorResponse
 
 app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory=FRONTEND_PATH), name="static")
@@ -18,12 +18,13 @@ async def get_home():
 
 
 @app.get("/api/bacon_distance", response_model=BaconDistanceResponse)
-async def handle_bacon_distance_request(request: BaconDistanceRequest):
+async def handle_bacon_distance_request(
+    actor_name: str = Query(...)) -> BaconDistanceResponse:
     try:
         bacon_distance = calc_bacon_distance(
-            request.actor_name, app.state.movies_dataset
+            actor_name, app.state.movies_dataset
         )
-        return {"bacon_distance": bacon_distance}
+        return BaconDistanceResponse(bacon_distance=bacon_distance)
     except ValueError as e:
         raise HTTPException(
             status_code=400, detail=ErrorResponse(message=str(e)).model_dump()
--- bacondistance/api/schemes.py
+++ bacondistance/api/schemes.py
@@ -1,10 +1,6 @@
 from pydantic import BaseModel
 
 
-class BaconDistanceRequest(BaseModel):
-    actor_name: str
-
-
 class BaconDistanceResponse(BaseModel):
     bacon_distance: str
 
